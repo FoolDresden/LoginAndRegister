a673cf481f89eae65d33b1cc87c876e5ca48cb61
ShreyamKr <kumarshreyam0409@gmail.com>
2018-06-28 06:56:46 +0530

    Added custom error messages and corrected Session tokens

--- server.js
+++ server.js
@@ -18,10 +18,15 @@ app.use(session({secret: 'lala'}))
 // var multipartMiddleware = multipart();
 
 app.get('/', (req, res)=>{
-  if(!req.session.user){
-    req.session.user = 'Guest';
+  if(!req.session.User){
+    req.session.User = {
+      privilege: 'guest',
+      courses: [],
+      user: 'Guest',
+      pwd: 'password',
+    };
   }
-  res.render(path.join(__dirname, '/views/home.hbs'), {user: req.session.user});
+  res.render(path.join(__dirname, '/views/home.hbs'), {user: req.session.User.user});
 });
 
 app.post('/register', function(req, res){
@@ -35,8 +40,9 @@ app.post('/register', function(req, res){
       // console.log(err);
       return res.status(400).send(err);
     }else{
-      req.session.user = req.body.user;
-      req.session.pwd = req.body.pwd;
+      // req.session.user = req.body.user;
+      // req.session.pwd = req.body.pwd;
+      req.session.User = doc;
       res.send(doc);
     }
   });
@@ -46,10 +52,10 @@ app.get('/register', (req, res)=>{
   res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
-app.get('/logged', (req, res)=>{
+app.get('/profile', (req, res)=>{
   console.log("Redirected to logged");
-  console.log("req.session.user: ",req.session.user);
-  res.render(path.join(__dirname, '/views/logged.hbs'), {username: req.session.user});
+  console.log("req.session.user: ",req.session.User.user);
+  res.render(path.join(__dirname, '/views/logged.hbs'), {username: req.session.User.user});
   // res.sendFile(path.join(__dirname, '/views/home.html'));
   console.log("Hi. After render");
 });
@@ -60,8 +66,9 @@ app.post('/login', (req, res)=>{
   User.findOne({user, pwd}).then((doc)=>{
     if(doc){
       console.log("Success login");
-      req.session.user = user;
-      req.session.pwd = pwd;
+      req.session.User = doc;
+      // req.session.doc = doc;
+      // console.log(req.session.doc);
       res.set('text/plain').send("Success Login");
       // console.log(user);
       // console.log(pwd);
@@ -80,34 +87,44 @@ app.get('/login', (req, res)=>{
 });
 
 app.post('/courses', (req, res)=>{
-  var name = req.body.name;
-  var credits = req.body.credits;
-  var text = req.body.text;
-  var regid = req.body.regid;
-  var course = Course({
-    name,
-    credits,
-    text,
-    regid,
-  });
-  course.save().then((doc)=>{
-    res.send(doc);
-  }, (err)=>{
-    res.status(400).send(err);
-  });
+  if(!req.session.User || req.session.User.privilege==='guest'
+    || req.session.User.privilege==='user'){
+      res.render(path.join(__dirname, '/views/error.hbs'), {
+        error: "Need to sign in to get courses page",
+      });
+      return;
+    }else if(req.session.User.privilege==='admin'){
+      var name = req.body.name;
+      var credits = req.body.credits;
+      var text = req.body.text;
+      var regid = req.body.regid;
+      var course = Course({
+        name,
+        credits,
+        text,
+        regid,
+      });
+      course.save().then((doc)=>{
+        res.send(doc);
+      }, (err)=>{
+        res.status(400).send(err);
+      });
+    }
 });
 
 app.get('/courses', (req, res)=>{
-  if(!req.session.user){
-    res.sendFile(path.join(__dirname, '/views/error.html'));
+  if(!req.session.User || req.session.User.user==='Guest'){
+    res.render(path.join(__dirname, '/views/error.hbs'), {
+      error: "Need to sign in to get courses page",
+    });
     return;
   }
-  User.findOne({user: req.session.user}).then((doc)=>{
+  User.findOne({user: req.session.User.user}).then((doc)=>{
     console.log("User found");
     // res.send(doc);
     res.render(path.join(__dirname, '/views/registeredCourses.hbs'), {
       courses: doc.courses,
-      username: req.session.user,
+      username: req.session.User.user,
     });
   }, (err)=>{
     res.status(400).send(err);
@@ -127,9 +144,6 @@ app.get('/courses/:id', (req, res)=>{
   });
 });
 
-app.get('/erp', (req, res)=>{
-  
-});
 
 app.listen(3000, ()=>{
   console.log("Check 3000");
